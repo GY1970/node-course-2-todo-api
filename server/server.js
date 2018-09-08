3b01eaeac6cf50fb44f92eaa65cfc95b645eeafa
GY1970 <gy1970@yahoo.com>
2018-09-08 13:32:06 -1000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -27,6 +28,34 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/12341234
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Valid id using isValid
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send(); // 404 - send back empty send
+  }
+
+  // findById
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send(); // if no todo - send back 404 with empty body
+    }
+    // success
+    res.send({ todo }); // if todo - send it back
+  }).catch((e) => {
+    res.send(400).send(); // 400 - and send empty body back
+  });
+
+
+
+  // error
+
+
+
 });
 
 app.listen(3000, () => {
